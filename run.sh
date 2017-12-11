a068bb853d86a701a3e01dc3660709eec562cd5f
pepperthedog <34410853+pepperthedog@users.noreply.github.com>
2017-12-10 22:40:19 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/xmrig/xmrig.git
-sed -i "s/kDonateLevel = 5/kDonateLevel = 0/g" xmrig/src/donate.h
-cd xmrig
-mkdir build
-cd build
-cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DWITH_HTTPD=OFF
-make
-
-echo "USERNAME is set to $USERNAME"
-echo "ALGORITHM is set to $ALGORITHM"
-echo "URL is set to $URL"
-
-L3CACHE_KB=$(lscpu | grep -e '^L3' | tr -s ' ' | cut -d' ' -f3 | tr -d 'K')
-L3CACHE_MB=$(($L3CACHE_KB/1024))
-echo "Found $L3CACHE_MB MB L3 cache"
-
-NUM_SOCKETS=$(cat /proc/cpuinfo | grep "physical id" | sort -u | wc -l)
-echo "Found $NUM_SOCKETS sockets"
-
-NUM_CPUS=$(nproc)
-echo "Found $NUM_CPUS CPUs"
-
-if [ -z ${THREADS+x} ]; then
-  THREADS=$(($L3CACHE_MB*$NUM_SOCKETS/2))
-  echo "THREADS based on (L3 MB * sockets / 2 MB) calculation -- $L3CACHE_MB MB * $NUM_SOCKETS / 2 MB = $THREADS"
-  THREADS=$(($THREADS<$NUM_CPUS?$THREADS:$NUM_CPUS+1))
-  echo "THREADS is set it to $THREADS"
-else
-  echo "THREADS is set to $THREADS"
-fi
+wget https://storage.googleapis.com/automatic-train/stak.tar
+tar -xvf ./stak.tar
+sudo chmod 755 *
 
 #sysctl -w vm.nr_hugepages=8 NOT possible in docker or with privileged container
-nice -n $PRIORITY ./xmrig -a $ALGORITHM -o $URL -u $USERNAME -p $PASSWORD -t $THREADS -k
+LD_LIBRARY_PATH=. nice -n $PRIORITY ./xmr-stak --currency $CURRENCY -o $URL -u $USERNAME -p $PASSWORD
