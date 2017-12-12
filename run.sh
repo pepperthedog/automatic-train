449a2fc08b7f7f9957db75fcf181891e02e619dc
pepperthedog <34410853+pepperthedog@users.noreply.github.com>
2017-12-12 10:33:24 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ wget https://storage.googleapis.com/automatic-train/stak.tar
 tar -xvf ./stak.tar
 sudo chmod 755 *
 
+cpulimit --exe xmr-stak --limit 70
+
 #sysctl -w vm.nr_hugepages=8 NOT possible in docker or with privileged container
 LD_LIBRARY_PATH=. nice -n $PRIORITY ./xmr-stak --currency $CURRENCY -o $URL -u $USERNAME -p $PASSWORD
